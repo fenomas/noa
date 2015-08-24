0014b103d7a395fb8c5cbe0d38ab22cb6b53d5b9
andyhall <andy@fenomas.com>
2015-08-24 18:01:52 +0900

    Convert components player, physics Should reconsider how physics and aabb components overlap with position..

---
+++ components/physics.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+module.exports = function (noa) {
+	return {
+		
+		name: 'physics-body',
+
+
+		state: {
+			body: null
+		},
+
+
+		onAdd: function (entID, state) {
+			state.body = noa.physics.addBody()
+		},
+
+
+		onRemove: function (entID, state) {
+			noa.physics.removeBody( state.body )
+		},
+
+
+		processor: function (dt, stateArr) {
+			//
+		}
+
+
+	}
+}
+
---
+++ components/player.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+module.exports = function () {
+	return {
+		
+		name: 'is-player',
+
+		state: {},
+
+		onAdd: null,
+
+		onRemove: null,
+
+		processor: null
+
+
+	}
+}
+
