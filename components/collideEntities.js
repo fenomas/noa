0992d3521de3c5748aff563931d9e311380d17b6
andyhall <andy@fenomas.com>
2015-10-15 16:19:52 +0900

    Overhaul collideEntities component to work right, and have categories/masks

--- components/collideEntities.js
+++ components/collideEntities.js
@@ -2,78 +2,96 @@
 
 var boxIntersect = require('box-intersect')
 
-var intervals = [],
-	ids = [],
-	idToi = []
+var noa
 
-module.exports = function (noa) {
+
+/**
+ * 
+ * 	Every frame, entities with this component will get checked for colliions
+ * 
+ *   * collideBits: category for this entity
+ *   * collideMask: categories this entity collides with
+ *   * callback: function(other_id) - called when `own.collideBits & other.collideMask` is true
+/*
+
+
+/*
+ * 
+ * 		Notes:
+ * 	normal entity (e.g. monster) probably wants bits=1; mask=1
+ *  bullets want bits=0, mask=1  (collide with things, but things don't collide back)
+ *  something with no callback (e.g. critter) probably wants bits=1, mask=0
+ * 
+ * 
+ * TODO: could optimize this by doing bipartite checks for certain groups 
+ * 		instead of one big collision check for everything
+ * 		(IF there's a bottleneck...)
+ * 
+*/
+
+ 
+
+module.exports = function (_noa) {
+	noa = _noa
+	
 	return {
 
 		name: 'collide-entities',
 
 		state: {
-			callback: null
+			collideBits: 1|0,
+			collideMask: 1|0,
+			callback: null,
 		},
 
-		onAdd: function (eid, state) {
-			// add collide handler for physics engine to call
-			var ents = noa.entities
-			if (ents.hasComponent(eid, ents.components.physics)) {
-				var body = ents.getPhysicsBody(eid)
-				body.onCollide = function (impulse) {
-					var cb = ents.getData(eid, 'collide-terrain').callback
-					if (cb) cb(impulse, eid)
-				}
-			}
-		},
+		onAdd: null, 
 
-		onRemove: function (eid, state) {
-			var ents = noa.entities
-			if (ents.hasComponent(eid, ents.components.physics)) {
-				ents.getPhysicsBody(eid).onCollide = null
-			}
-		},
-
+		onRemove: null,
+		
 
 		processor: function entityCollider(dt, states) {
 			// populate data struct that boxIntersect looks for
-			populateIntervals(intervals, ids, idToi, states, noa.entities)
+			var ints = populateIntervals(states)
+			
 			// find collisions and call callbacks
-			var collideEnt = noa.entities.components.collideEntities
-			boxIntersect(intervals, function (i, j) {
-				var iid = ids[i]
-				var jid = ids[j]
-				// var ihandler = states[idToi[iid]].callback
-				var ihandler = noa.entities.getData(iid, collideEnt).callback
-				if (ihandler) ihandler(iid, jid)
-				// var jhandler = states[idToi[jid]].callback
-				var jhandler = noa.entities.getData(jid, collideEnt).callback
-				if (jhandler) jhandler(jid, iid)
-				// if (iid==0 || jid==0) throw new Error()
+			boxIntersect(ints, function (i, j) {
+				var istate = states[i]
+				var jstate = states[j]
+				
+				if (istate.collideMask & jstate.collideBits) {
+					if (istate.callback) istate.callback(jstate.__id)
+				}
+				if (jstate.collideMask & istate.collideBits) {
+					if (jstate.callback) jstate.callback(istate.__id)
+				}
+				
 			})
 		}
-
-
-
+		
+		
 	}
 }
 
 
+
+
 // implementation:
 
-function populateIntervals(intervals, ids, idToi, states, entitites) {
+var intervals = []
+
+function populateIntervals(states) {
 	// grow/shrink [lo, lo, hi, hi] array entries
 	// optimized to common case where states.length is the same as last time
 	while (intervals.length < states.length) {
 		intervals.push(new Float32Array(6))
 	}
 	intervals.length = states.length
-	ids.length = states.length
-	idToi.length = states.length
+	
+	var ents = noa.entities
 	// populate [lo, lo, lo, hi, hi, hi] arrays
 	for (var i = 0; i < states.length; i++) {
 		var id = states[i].__id
-		var box = entitites.getAABB(id)
+		var box = ents.getAABB(id)
 		var lo = box.base
 		var hi = box.max
 		var arr = intervals[i]
@@ -81,10 +99,11 @@ function populateIntervals(intervals, ids, idToi, states, entitites) {
 			arr[j] = lo[j]
 			arr[j + 3] = hi[j]
 		}
-		ids[i] = id
-		idToi[id] = i
 	}
+	
+	return intervals
 }
+
 
 
 
