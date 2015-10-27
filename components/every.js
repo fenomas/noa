7d252cf77fea84393f30aaed755e3e0e062b553a
andyhall <andy@fenomas.com>
2015-10-27 14:10:41 +0900

    Replace `countdown` component with `every`

---
+++ components/every.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+module.exports = function (noa) {
+	return {
+		
+		name: 'every',
+
+		state: {
+			every:		100.0, //ms
+			callback:	null,
+			_ct: 0.0
+		},
+
+		onAdd: null,
+
+		onRemove: null,
+
+		processor: function everyProcessor(dt, states) {
+			for (var i=0; i<states.length; i++) {
+				var state = states[i]
+				state._ct += dt
+				if (state._ct > state.every) {
+					state._ct -= state.every
+					state.callback()
+				}
+			}
+		}
+
+
+	}
+}
+
