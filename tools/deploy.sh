648cf197ae32b919e916ebb06765cf61fe5a4e55
andyhall <andy@fenomas.com>
2016-03-30 15:26:17 +0900

    Fixing deploy script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,5 +4,7 @@ git checkout gh-pages
 git merge -X theirs master -m "Merge branch 'master' into gh-pages"
 webpack examples/hello-world/index.js examples/hello-world/bundle.js
 webpack examples/test/index.js examples/test/bundle.js
-git commit bundle.js -m "Regenerate bundle.js using deploy.sh"
+git add examples/hello-world/bundle.js
+git add examples/test/bundle.js
+git commit -m "Regenerate bundles using deploy.sh"
 git checkout master
