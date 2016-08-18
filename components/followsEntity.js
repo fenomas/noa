e47580a0438b6084c4c6471d95b44813020f1eed
andyhall <andy@fenomas.com>
2016-08-18 13:16:45 +0900

    Fix followsEntity to catch case where followed entity gets deleted
    Bumps ent-comp dependency for new api

--- components/followsEntity.js
+++ components/followsEntity.js
@@ -10,44 +10,52 @@ var _tempVec = vec3.create()
  */
 
 module.exports = function (noa) {
-	
+
 	return {
 
 		name: 'followsEntity',
 
 		state: {
-			entity: 0|0,
+			entity: 0 | 0,
 			offset: null,
 		},
 
-		onAdd: function(eid, state) {
+		onAdd: function (eid, state) {
 			var off = vec3.create()
 			state.offset = (state.offset) ? vec3.copy(off, state.offset) : off
 		},
 
 		onRemove: null,
-		
-		
+
+
 		// on tick, copy over regular positions
 		system: function followEntity(dt, states) {
 			var pos = _tempVec
-			for (var i=0; i<states.length; i++) {
-				var state = states[i]
-				var self = noa.ents.getPositionData(state.__id)
-				var other = noa.ents.getPositionData(state.entity).position
-				vec3.add(pos, other, state.offset)
-				self.setPosition(pos[0], pos[1], pos[2])
-			}
-		},
-		
-		
-		// on render, copy over render positions
-		renderSystem: function followEntityMesh(dt, states) {
-			for (var i=0; i<states.length; i++) {
+			for (var i = 0; i < states.length; i++) {
 				var state = states[i]
 				var self = noa.ents.getPositionData(state.__id)
 				var other = noa.ents.getPositionData(state.entity)
-				vec3.add(self.renderPosition, other.renderPosition, state.offset)
+				if (other) {
+					vec3.add(pos, other.position, state.offset)
+					self.setPosition(pos[0], pos[1], pos[2])
+				} else {
+					noa.ents.removeComponentLater(state.__id, noa.ents.names.followsEntity)
+				}
+			}
+		},
+
+
+		// on render, copy over render positions
+		renderSystem: function followEntityMesh(dt, states) {
+			for (var i = 0; i < states.length; i++) {
+				var state = states[i]
+				var self = noa.ents.getPositionData(state.__id)
+				var other = noa.ents.getPositionData(state.entity)
+				if (other) {
+					vec3.add(self.renderPosition, other.renderPosition, state.offset)
+				} else {
+					noa.ents.removeComponentLater(state.__id, noa.ents.names.followsEntity)
+				}
 			}
 		}
 
