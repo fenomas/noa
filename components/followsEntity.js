d0b4a71a999b847209188d3055b8fa796bf865d2
andyhall <andy@fenomas.com>
2016-10-27 14:57:34 +0900

    Make followsEntity component apply itself when first added

--- components/followsEntity.js
+++ components/followsEntity.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
 var vec3 = require('gl-vec3')
 var _tempVec = vec3.create()
 
 
-/**
+/*
  * Indicates that an entity should be moved to another entity's position each tick,
  * possibly by a fixed offset, and the same for renderPositions each render
- */
+*/
 
 module.exports = function (noa) {
 
@@ -23,6 +23,8 @@ module.exports = function (noa) {
 		onAdd: function (eid, state) {
 			var off = vec3.create()
 			state.offset = (state.offset) ? vec3.copy(off, state.offset) : off
+			updatePosition(state)
+			updateRenderPosition(state)
 		},
 
 		onRemove: null,
@@ -30,17 +32,8 @@ module.exports = function (noa) {
 
 		// on tick, copy over regular positions
 		system: function followEntity(dt, states) {
-			var pos = _tempVec
 			for (var i = 0; i < states.length; i++) {
-				var state = states[i]
-				var self = noa.ents.getPositionData(state.__id)
-				var other = noa.ents.getPositionData(state.entity)
-				if (other) {
-					vec3.add(pos, other.position, state.offset)
-					self.setPosition(pos[0], pos[1], pos[2])
-				} else {
-					noa.ents.removeComponentLater(state.__id, noa.ents.names.followsEntity)
-				}
+				updatePosition(states[i])
 			}
 		},
 
@@ -48,22 +41,38 @@ module.exports = function (noa) {
 		// on render, copy over render positions
 		renderSystem: function followEntityMesh(dt, states) {
 			for (var i = 0; i < states.length; i++) {
-				var state = states[i]
-				var self = noa.ents.getPositionData(state.__id)
-				var other = noa.ents.getPositionData(state.entity)
-				if (other) {
-					vec3.add(self.renderPosition, other.renderPosition, state.offset)
-				} else {
-					noa.ents.removeComponentLater(state.__id, noa.ents.names.followsEntity)
-				}
+				updateRenderPosition(states[i])
 			}
 		}
-
-
 	}
+
+
+
+	function updatePosition(state) {
+		var pos = _tempVec
+		var id = state.__id
+		var self = noa.ents.getPositionData(id)
+		var other = noa.ents.getPositionData(state.entity)
+		if (other) {
+			vec3.add(pos, other.position, state.offset)
+			self.setPosition(pos[0], pos[1], pos[2])
+		} else {
+			noa.ents.removeComponentLater(id, noa.noa.ents.names.followsEntity)
+		}
+	}
+
+	function updateRenderPosition(state) {
+		var pos = _tempVec
+		var id = state.__id
+		var self = noa.ents.getPositionData(id)
+		var other = noa.ents.getPositionData(state.entity)
+		if (other) {
+			vec3.add(self.renderPosition, other.renderPosition, state.offset)
+		} else {
+			noa.ents.removeComponentLater(id, noa.ents.names.followsEntity)
+		}
+	}
+
 }
-
-
-
 
 
