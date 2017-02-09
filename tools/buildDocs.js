bf9d8a0e1600cf2bb32f3aaf6d9f1292163a356b
andyhall <andy@fenomas.com>
2017-02-09 22:57:41 +0900

    Hacking on doc building workflow, to fix broken indentation in readme

--- tools/buildDocs.js
+++ tools/buildDocs.js
@@ -1,16 +1,26 @@
 
+var fs = require('fs')
 var markdox = require('markdox')
 
-markdox.process(
-	[
-		'index.js',
-		'lib/entities.js',
-		'lib/world.js'
-	],
-	{
-		output: 'README.md',
-		template: 'tools/doctemplate.ejs',
-		dox: {
-			skipSingleStar: true
-		}
+var fixtures = [
+	'index.js',
+	'lib/entities.js',
+	'lib/world.js'
+]
+
+var options = {
+	output: 'README.md',
+	template: 'tools/doctemplate.ejs',
+	dox: {
+		skipSingleStar: true
+	}
+}
+
+markdox.process(fixtures, options, function () {
+	header = fs.readFileSync(__dirname + '/tools/readme_header.md')
+	body = fs.readFileSync(__dirname + '/README.md')
+	var joined = header + body
+	fs.writeFile(__dirname + '/README.md', joined, function (err) {
+		if (err) return console.log(err);
 	})
+})
