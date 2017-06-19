fb85b5d2ae1c41a40908889a452b5a2bda44ae60
andyhall <andy@fenomas.com>
2017-06-19 14:27:18 +0900

    Possible fix for #31 - draws entity shadows offset from ground by a value depending on distance from the camera.

--- components/shadow.js
+++ components/shadow.js
@@ -1,19 +1,20 @@
 'use strict'
 
+var vec3 = require('gl-vec3')
 var shadowDist
 
 module.exports = function (noa, dist) {
 
 	shadowDist = dist
 
-    // create a mesh to re-use for shadows
+	// create a mesh to re-use for shadows
 	var scene = noa.rendering.getScene()
-    var disc = BABYLON.Mesh.CreateDisc('shadowMesh', 0.75, 30, scene)
-    disc.rotation.x = Math.PI / 2
-    disc.material = noa.rendering.makeStandardMaterial('shadowMat')
-    disc.material.diffuseColor = BABYLON.Color3.Black()
-    disc.material.specularColor = BABYLON.Color3.Black()
-    disc.material.alpha = 0.5
+	var disc = BABYLON.Mesh.CreateDisc('shadowMesh', 0.75, 30, scene)
+	disc.rotation.x = Math.PI / 2
+	disc.material = noa.rendering.makeStandardMaterial('shadowMat')
+	disc.material.diffuseColor = BABYLON.Color3.Black()
+	disc.material.specularColor = BABYLON.Color3.Black()
+	disc.material.alpha = 0.5
 	disc.setEnabled(false)
 
 
@@ -38,6 +39,8 @@ module.exports = function (noa, dist) {
 
 
 		system: function shadowSystem(dt, states) {
+			var cpos = noa.rendering.getCameraPosition()
+			vec3.set(camPos, cpos.x, cpos.y, cpos.z)
 			var dist = shadowDist
 			for (var i = 0; i < states.length; i++) {
 				var state = states[i]
@@ -63,14 +66,16 @@ module.exports = function (noa, dist) {
 	}
 }
 
-var down = new Float32Array([0, -1, 0])
+var down = vec3.fromValues(0, -1, 0)
+var camPos = vec3.fromValues(0, 0, 0)
+var shadowPos = vec3.fromValues(0, 0, 0)
 
 function updateShadowHeight(id, mesh, size, shadowDist, noa) {
 	var ents = noa.entities
 	var dat = ents.getPositionData(id)
 	var loc = dat.position
 	var b = ents.getPhysicsBody(id)
-	var y, dist
+	var y
 	// set to entity position if entity standing on ground
 	if (b.resting[1] < 0) {
 		y = dat.renderPosition[1]
@@ -79,7 +84,15 @@ function updateShadowHeight(id, mesh, size, shadowDist, noa) {
 		if (pick) y = pick.position[1]
 	}
 	if (y !== undefined) {
-		mesh.position.y = y + 0.05
+		y = Math.round(y) // pick results get slightly countersunk
+		// set shadow slightly above ground to avoid z-fighting
+		vec3.set(shadowPos, mesh.position.x, y, mesh.position.z)
+		var sqdist = vec3.squaredDistance(camPos, shadowPos)
+		// offset ~ 0.01 for nearby shadows, up to 0.1 at distance of ~40
+		var offset = 0.01 + 0.1 * (sqdist / 1600)
+		if (offset > 0.1) offset = 0.1
+		mesh.position.y = y + offset
+		// set shadow scale
 		var dist = loc[1] - y
 		var scale = size * 0.7 * (1 - dist / shadowDist)
 		mesh.scaling.copyFromFloats(scale, scale, scale)
