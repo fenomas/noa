c92cbfe25da5269a584a0f7eeda91e125acbd695
andyhall <andy@fenomas.com>
2018-01-10 13:51:35 +0900

    Workaround for nasty chrome/win10 pointerlock bug see: https://bugs.chromium.org/p/chromium/issues/detail?id=781182

--- src/lib/camera.js
+++ src/lib/camera.js
@@ -41,6 +41,9 @@ CameraController.prototype.updateForRender = function () {
 	// input state
 	var state = this.noa.inputs.state
 
+	// TODO: REMOVE EVENTUALLY
+	bugFix(state)
+
 	// Rotation: translate dx/dy inputs into y/x axis camera angle changes
 	var dx = this.rotationScale * state.dy * ((this.inverseY) ? -1 : 1)
 	var dy = this.rotationScale * state.dx
@@ -60,5 +63,28 @@ function clamp(value, to) {
 }
 
 
+
+// workaround for this Chrome 63 + Win10 bug
+// https://bugs.chromium.org/p/chromium/issues/detail?id=781182
+function bugFix(state) {
+	var dx = state.dx
+	var dy = state.dy
+	var wval = window.innerWidth / 6
+	var hval = window.innerHeight / 6
+	var badx = (Math.abs(dx) > wval && (dx / lastx) < -1)
+	var bady = (Math.abs(dy) > hval && (dy / lasty) < -1)
+	if (badx || bady) {
+		state.dx = lastx
+		state.dy = lasty
+		lastx = (dx > 0) ? 1 : -1
+		lasty = (dy > 0) ? 1 : -1
+	} else {
+		if (dx) lastx = dx
+		if (dy) lasty = dy
+	}
+}
+
+var lastx = 0
+var lasty = 0
 
 
